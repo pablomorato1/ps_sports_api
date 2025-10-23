47ff5202c44b18f80b427a23337f724ff64203cd
Allan Rodrigues <allan.rodrigues@jala.university>
2025-10-23 16:06:42 -0300

    feature: inicial configuration token_auth

--- db/migrate/20251023181641_devise_create_users.rb
+++ db/migrate/20251023181641_devise_create_users.rb
@@ -4,6 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name
+      t.string :username
+      t.string :role
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
