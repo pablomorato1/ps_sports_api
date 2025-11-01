1d4cd47fb259afadb1475c738c9e21f56944fc97
Allan Rodrigues <allan.rodrigues@jala.university>
2025-11-01 10:58:02 -0300

    fix: remoção da regra de negocíos não solicitada no model de user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  belongs_to :parent, class_name: 'User', optional: true
-  has_many :children, class_name: 'User', foreign_key: :parent_id, dependent: :nullify
-
   validates :nome, presence: true
   validates :role, inclusion: { in: %w[admin user], message: "%{value} não é um papel válido" }, allow_nil: true
 end
